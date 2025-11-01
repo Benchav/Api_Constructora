657b4236d346678e13e6324c9116f4e6b9dc7513
benchav <joshua44benja@gmail.com>
2025-10-31 22:40:48 -0600

    planos controllers

---
+++ src/controllers/planosController.js
@@ -0,0 +1,16 @@
+const planosService = require('../services/planosService');
+const makeController = require('./genericController');
+const { validateCreatePlano } = require('../models/plano');
+
+const controller = makeController(planosService);
+
+const originalCreate = controller.create;
+controller.create = (req, res) => {
+  const validation = validateCreatePlano(req.body);
+  if (!validation.ok) {
+    return res.status(400).json({ message: 'Faltan campos requeridos', missing: validation.missing });
+  }
+  return originalCreate(req, res);
+};
+
+module.exports = controller;
