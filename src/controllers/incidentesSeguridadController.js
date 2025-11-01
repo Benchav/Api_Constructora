9d722adb8cd1035f0a5db2cb9222e058432dbe94
benchav <joshua44benja@gmail.com>
2025-10-31 22:44:59 -0600

    seguridad controller

---
+++ src/controllers/incidentesSeguridadController.js
@@ -0,0 +1,17 @@
+const incidentesSeguridadService = require('../services/incidentesSeguridadService');
+const makeController = require('./genericController');
+const { validateCreateIncidenteSeguridad } = require('../models/incidenteSeguridad');
+
+const controller = makeController(incidentesSeguridadService);
+
+
+const originalCreate = controller.create;
+controller.create = (req, res) => {
+  const validation = validateCreateIncidenteSeguridad(req.body);
+  if (!validation.ok) {
+    return res.status(400).json({ message: 'Faltan campos requeridos', missing: validation.missing });
+  }
+  return originalCreate(req, res);
+};
+
+module.exports = controller;
