3865e6457dd739796a2f19083f3be217bd865094
benchav <joshua44benja@gmail.com>
2025-10-31 19:27:33 -0600

    storage

---
+++ src/storage/inMemoryDb.js
@@ -0,0 +1,29 @@
+const { initialUsuarios } = require('../data/usuarios');
+const { initialProyectos } = require('../data/proyectos');
+const { initialInventarioObra } = require('../data/inventario');
+const { initialEmpleados } = require('../data/empleados');
+const { initialFinanzas } = require('../data/finanzas');
+const { initialLicitaciones } = require('../data/licitaciones');
+const { initialPlanos } = require('../data/planos');
+const { initialReportesDiarios } = require('../data/reportesDiarios');
+const { initialSolicitudesMateriales } = require('../data/solicitudesMateriales');
+const { initialSolicitudesDinero } = require('../data/solicitudesDinero');
+const { initialOrdenesCompra } = require('../data/ordenesCompra');
+const { initialInspeccionesCalidad } = require('../data/inspeccionesCalidad');
+const { initialIncidentesSeguridad } = require('../data/incidentesSeguridad');
+
+module.exports = {
+  initialUsuarios,
+  initialProyectos,
+  initialInventarioObra,
+  initialEmpleados,
+  initialFinanzas,
+  initialLicitaciones,
+  initialPlanos,
+  initialReportesDiarios,
+  initialSolicitudesMateriales,
+  initialSolicitudesDinero,
+  initialOrdenesCompra,
+  initialInspeccionesCalidad,
+  initialIncidentesSeguridad
+};
