f694f6a68c5a1dc13cfc1b528f054b99c0e1e570
benchav <joshua44benja@gmail.com>
2025-10-31 19:09:33 -0600

    update data

---
+++ src/data/licitaciones.js
@@ -0,0 +1,7 @@
+const initialLicitaciones = [
+  { id: "l1", nombre: "Hospital Regional", estado: "Presentada", monto: 2500000, fechaLimite: "2025-11-15" },
+  { id: "l2", nombre: "Puente Sur", estado: "En Preparacion", monto: 800000, fechaLimite: "2025-11-30" },
+  { id: "l3", nombre: "Complejo Deportivo", estado: "Ganada", monto: 1800000, fechaLimite: "2025-10-01" }
+];
+
+module.exports = { initialLicitaciones };
