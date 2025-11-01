1e2e758388386115de9b6680040373822b85a131
benchav <joshua44benja@gmail.com>
2025-10-31 19:30:13 -0600

    news store fun

--- src/storage/inMemoryDb.js
+++ src/storage/inMemoryDb.js
@@ -1,18 +1,7 @@
-const { initialUsuarios } = require('../data/usuarios');
-const { initialProyectos } = require('../data/proyectos');
-const { initialInventarioObra } = require('../data/inventario');
-const { initialEmpleados } = require('../data/empleados');
-const { initialFinanzas } = require('../data/finanzas');
-const { initialLicitaciones } = require('../data/licitaciones');
-const { initialPlanos } = require('../data/planos');
-const { initialReportesDiarios } = require('../data/reportesDiarios');
-const { initialSolicitudesMateriales } = require('../data/solicitudesMateriales');
-const { initialSolicitudesDinero } = require('../data/solicitudesDinero');
-const { initialOrdenesCompra } = require('../data/ordenesCompra');
-const { initialInspeccionesCalidad } = require('../data/inspeccionesCalidad');
-const { initialIncidentesSeguridad } = require('../data/incidentesSeguridad');
+// src/storage/inMemoryDb.js
+// Estado mutable en memoria. Inicializa con los seeds desde src/data/index.js
 
-module.exports = {
+const {
   initialUsuarios,
   initialProyectos,
   initialInventarioObra,
@@ -26,4 +15,73 @@ module.exports = {
   initialOrdenesCompra,
   initialInspeccionesCalidad,
   initialIncidentesSeguridad
+} = require('../data'); // src/data/index.js que exporta todo
+
+// Inicializamos el "estado" en memoria copiando los seeds
+let usuarios = Array.isArray(initialUsuarios) ? [...initialUsuarios] : [];
+let proyectos = Array.isArray(initialProyectos) ? [...initialProyectos] : [];
+let inventario = Array.isArray(initialInventarioObra) ? [...initialInventarioObra] : [];
+let empleados = Array.isArray(initialEmpleados) ? [...initialEmpleados] : [];
+let finanzas = Array.isArray(initialFinanzas) ? [...initialFinanzas] : [];
+let licitaciones = Array.isArray(initialLicitaciones) ? [...initialLicitaciones] : [];
+let planos = Array.isArray(initialPlanos) ? [...initialPlanos] : [];
+let reportes = Array.isArray(initialReportesDiarios) ? [...initialReportesDiarios] : [];
+let solicitudesMateriales = Array.isArray(initialSolicitudesMateriales) ? [...initialSolicitudesMateriales] : [];
+let solicitudesDinero = Array.isArray(initialSolicitudesDinero) ? [...initialSolicitudesDinero] : [];
+let ordenesCompra = Array.isArray(initialOrdenesCompra) ? [...initialOrdenesCompra] : [];
+let inspecciones = Array.isArray(initialInspeccionesCalidad) ? [...initialInspeccionesCalidad] : [];
+let incidentes = Array.isArray(initialIncidentesSeguridad) ? [...initialIncidentesSeguridad] : [];
+
+module.exports = {
+  // Usuarios
+  getUsuarios: () => usuarios,
+  setUsuarios: (newArr) => { usuarios = newArr; },
+
+  // Proyectos
+  getProyectos: () => proyectos,
+  setProyectos: (newArr) => { proyectos = newArr; },
+
+  // Inventario
+  getInventario: () => inventario,
+  setInventario: (newArr) => { inventario = newArr; },
+
+  // Empleados
+  getEmpleados: () => empleados,
+  setEmpleados: (newArr) => { empleados = newArr; },
+
+  // Finanzas
+  getFinanzas: () => finanzas,
+  setFinanzas: (newArr) => { finanzas = newArr; },
+
+  // Licitaciones
+  getLicitaciones: () => licitaciones,
+  setLicitaciones: (newArr) => { licitaciones = newArr; },
+
+  // Planos
+  getPlanos: () => planos,
+  setPlanos: (newArr) => { planos = newArr; },
+
+  // Reportes diarios
+  getReportes: () => reportes,
+  setReportes: (newArr) => { reportes = newArr; },
+
+  // Solicitudes materiales
+  getSolicitudesMateriales: () => solicitudesMateriales,
+  setSolicitudesMateriales: (newArr) => { solicitudesMateriales = newArr; },
+
+  // Solicitudes dinero
+  getSolicitudesDinero: () => solicitudesDinero,
+  setSolicitudesDinero: (newArr) => { solicitudesDinero = newArr; },
+
+  // Ordenes de compra
+  getOrdenesCompra: () => ordenesCompra,
+  setOrdenesCompra: (newArr) => { ordenesCompra = newArr; },
+
+  // Inspecciones
+  getInspecciones: () => inspecciones,
+  setInspecciones: (newArr) => { inspecciones = newArr; },
+
+  // Incidentes
+  getIncidentes: () => incidentes,
+  setIncidentes: (newArr) => { incidentes = newArr; }
 };
